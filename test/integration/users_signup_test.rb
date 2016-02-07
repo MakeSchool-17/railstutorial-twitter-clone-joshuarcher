c14e5828928104f72eaca8d7ccd19ca2bb5d8a7b
Joshua Archer <josharcher@me.com>
2016-02-06 22:59:24 -0800

    save before ch8

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -26,4 +26,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/show'
   end
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name: "",
+                               email: "user@invalid",
+                               password: "foo",
+                               password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+    # this is because there are classes and ids on the div tags
+    # of the error html in new.html render
+
+  end
+
 end
